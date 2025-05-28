ad51a458cce6dc74991cbe6a4e333cbec19f8d38
Hernan Leiva <hleiva@hotmail.com>
2025-05-28 18:44:38 +0200

    fixed script path in index

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", 
+  base: "/meeting-calendar/", // 🔹 Se till att detta matchar ditt repo-namn
 });
